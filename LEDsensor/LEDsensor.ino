b0611138dd1e3cb32a2434410a4fad3e6093b72b
AndreasLanghammer <andreaslangahmmer47@gmail.com>
2016-01-21 19:00:18 +0100

    Improvments and bugfix
    Some improvments in code structure and samler bugfixing in longDelay

--- LEDsensor/LEDsensor.ino
+++ LEDsensor/LEDsensor.ino
@@ -9,37 +9,45 @@
   int sensor = A0; 
   int LED = 10; 
   int Vref = 11;
-  int transitionVal = 600;
+  int transitionVal = 800;
   int delayVal = 0;
   
   void setup(){
-    pinMode(LED, OUTPUT); //define Pin 10 as OUTPUT
-    pinMode(Vref, OUTPUT); // define Pin 11 as OUTPUT only used to set Vref (logic 1)
-  }
+    pinMode(LED, OUTPUT);              // Define Pin 10 as OUTPUT
+    pinMode(Vref, OUTPUT);             // Define Pin 11 as OUTPUT only used to set Vref (logic 1)
+    delay(5000);                       // Delay-Time to start monitor  
+}
   // Polling the value from Analog 0 and makes desicions
   void loop(){
-     digitalWrite(Vref, HIGH); // Set Vref
-     int sensVal; // Used to store Analog value from sensor
-     sensVal = analogRead(sensor); // Stores the value in sensVal
-     Serial.print("Sensor Value: "); // The next two rows only prints the value in the console.
+     digitalWrite(Vref, HIGH);         // Set Vref
+     int sensVal;                      // Used to store Analog value from sensor
+     sensVal = analogRead(sensor);     // Stores the value in sensVal
+     Serial.print("Sensor Value: ");   // The next two rows only prints the value in the console.
      Serial.print(sensVal);
-     Serial.print("\n");
-     delayVal = (sensor * 100); //Takes the value and multiplies it with 100 so the polling does not occur so often.
-     delay(1000); // Delay 1 s
+     Serial.print("\n");               // New line
+     delayVal = (sensVal / 10);        // Takes the value and multiplies it with 100 so the polling does not occur so often.
+     Serial.print("delayVal : ");
+     Serial.print(delayVal);
+     Serial.print("\n");               // New line
+     delay(1000);                      // Delay 1 s
      
      //The logic part
      if(sensVal < transitionVal){
-       digitalWrite(LED, HIGH); // LED on
+       digitalWrite(LED, HIGH);        // LED on
      }else{
-       digitalWrite(LED, LOW); // LED off
+       digitalWrite(LED, LOW);         // LED off
      }
-     longDelay(delayVal); // Pause delayVal seconds
+     longDelay(delayVal);              // Pause delayVal seconds
   }
   
  // A delay for handling greater periods of pauses.
-  void longDelay(int delVal){
+  void longDelay(int delayVal){
      int i = 0;
-     for(i = 0; i != delVal; i++){
+     Serial.print("Delay values: ");
+     for(i = 0; i != delayVal; i++){
+        Serial.print(i);                // Printing out every second in monitor
+        Serial.print(", ");
         delay(1000);
      }
+     Serial.print("\n");                // New line
   }
